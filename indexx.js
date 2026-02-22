32f82a623a2a732628c3e95f57df9199740090a2
Tainá Marques Salami <tainamarquesupf@gmail.com>
2026-02-22 18:14:19 -0300

    fundamentos 003

--- indexx.js
+++ indexx.js
@@ -1,17 +1,23 @@
 // arrays, objetos
-let meta = {
-    value: 'ler um livro por mês',
-    address: { street: 2, number: 3 },
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }
-}
 
-meta.value = "não é ler livro"
-meta.log(meta.value)
 
 // Function (fora do objeto, arrow function)
 //const criarMeta = () => {}
 //ou 
 //function criaMeta() {}
+
+
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20min por dia",
+        checked: false
+    }
+]
+
+console.log(metas[0].value)
